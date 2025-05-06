a4876f31e540e4b434d6222e288cb2300f1d35d1
hackanonimous <esi.segurity@gmail.com>
2025-05-05 21:09:55 -0500

    feat:integracion con IA

--- src/articulo/articulo.module.ts
+++ src/articulo/articulo.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ArticuloService } from './articulo.service';
 import { ArticuloController } from './articulo.controller';
+import { IaIntegrationService } from './ia-integration.service';
 import { Articulo } from './entities/articulo.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuarioModule } from 'src/usuario/usuario.module';
@@ -8,6 +9,6 @@ import { UsuarioModule } from 'src/usuario/usuario.module';
 @Module({
   imports: [TypeOrmModule.forFeature([Articulo]),UsuarioModule],
   controllers: [ArticuloController],
-  providers: [ArticuloService],
+  providers: [ArticuloService, IaIntegrationService],
 })
 export class ArticuloModule { }
